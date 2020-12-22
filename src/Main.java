4e99a754fde567f0d9813270b423952b529b232e
Lyubov Zabavina <zabavinal@yandex.ru>
2020-12-23 00:03:41 +0300

    .gitignore & Main.java created

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 1460_55;
+        int rubles = 20;
+        int miles = amount / rubles / 100;
+        System.out.println(miles);
+    }
+}
